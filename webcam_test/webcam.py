e1b9606732d01129271efa15c3e59240fcdf8889
Kenta Yonekura(a.k.a. yoneken) <yoneken@ieee.org>
2019-11-15 00:59:40 +0900

    Alternative input (camera or file)

--- webcam_test/webcam.py
+++ webcam_test/webcam.py
@@ -1,19 +1,53 @@
 import cv2
+import skvideo.io
+import sys, argparse
 
-def capture(device_no = 0):
-    cap = cv2.VideoCapture(device_no)
-
+def process(cap):
     while True:
-        ret, frame = cap.read()
-        cv2.imshow('cap', frame)
+        try:
+            if isinstance(cap, type(cv2.VideoCapture())): 
+                ret, frame = cap.read()
+            else:
+                frame = next(cap)
+                frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-        key = cv2.waitKey(1)
-        if key == 27: # ESC
-            cv2.imwrite('/webcam_test/cap.jpg', frame)
+            cv2.imshow('cap', frame)
+
+            key = cv2.waitKey(1)
+            if key == 27: # ESC
+                cv2.imwrite('/webcam_test/cap.jpg', frame)
+                break
+
+        except:
+            print(sys.exc_info())
             break
+
+
+def process_camera(dev_num: int = 0) -> int:
+    cap = cv2.VideoCapture(dev_num)
+
+    process(cap)
 
     cap.release()
     cv2.destroyAllWindows()
+    return 0
+
+def process_video(filename) -> int:
+    cap = skvideo.io.vreader(filename)
+
+    process(cap)
+
+    cv2.destroyAllWindows()
+    return 0
 
 if __name__ == "__main__":
-    capture()
+    parser = argparse.ArgumentParser(description='camera app')
+    parser.add_argument('--input', '-i', type=str, default='0', help='Device number or a video file')
+    args = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        process_camera()
+    elif args.input.isdecimal():
+        process_camera(int(args.input))
+    else:
+        process_video(args.input)
